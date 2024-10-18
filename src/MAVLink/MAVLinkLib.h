19cca13692ed8e53e88f642c150617889999e868
Lumy0726 <jinny9807@gmail.com>
2024-10-19 08:06:22 +0900

    FEAT: Implement base testing code for MESL_CRYPTO mavlink

--- src/MAVLink/MAVLinkLib.h
+++ src/MAVLink/MAVLinkLib.h
@@ -36,6 +36,17 @@ typedef enum : uint8_t {
 #define MAVLINK_COMM_NUM_BUFFERS 16
 #define MAVLINK_MAX_SIGNING_STREAMS MAVLINK_COMM_NUM_BUFFERS
 
+
+//␣-------------------------------------------------------
+// Setting compile options related with MESL_CRYPTO.
+//␣-------------------------------------------------------
+
+#define MESL_CRYPTO
+//#define MESL_MAVLINK_PARSE_FIX
+//#define MESL_MAVLINK_DEBUG
+
+
+
 #include <mavlink_types.h>
 
 #define MAVLINK_EXTERNAL_RX_STATUS
@@ -68,6 +79,123 @@ typedef enum : uint8_t {
 #endif
 
 #include <mavlink.h>
+
+
+
+// -------------------------------------------------------
+// Declare or implement, for MESL_CRYPTO related things.
+//   NOTE: 'MAVLINK_HELPER' is 'static inline' by default.
+// -------------------------------------------------------
+
+#ifdef MAVLINK_USE_CXX_NAMESPACE
+namespace mavlink {
+#endif
+
+#ifdef MESL_CRYPTO
+
+// @brief  Function to decide if MAVLink payload should be encrypted.
+//         Program that use MAVLink should implement this function.
+// @param  'len': payload length (can be 0).
+// @return 'MESL_CRYPTO_METHOD_XXX' (true),
+//           if MAVLink payload should be encrypted.
+//         Zero otherwise.
+MAVLINK_HELPER uint8_t mavlink_mesl_crypto_condition(
+		mavlink_status_t* status,
+		uint32_t msgid,
+		uint8_t system_id,
+		uint8_t component_id,
+		const char *payload,
+		uint8_t len
+		)
+{
+	// Use same crypto option, from rx MAVLink frame.
+	return status->mesl_crypto_method_rx;
+}
+
+// @brief  Function to encrypt MAVLink payload.
+//         Program that use MAVLink should implement this function.
+// @param  'crypto_method': method for encryption,
+//           value should be 'MESL_CRYPTO_METHOD_XXX'.
+// @param  'len': payload length (can be 0).
+// @return Payload length after encryption.
+// @note   "input_len == 0 && output_len == 0",
+//           will be considered as non-encryption.
+//         But, "input len != 0 && output_len == 0",
+//           or "output_len < 0 || output_len > maxlen",
+//           will be considered as error,
+//           MAVLink frame will be sent with zero payload length,
+//           and receiving side can report error,
+//           because the length is zero but encryption iflag is set.
+MAVLINK_HELPER int32_t mavlink_mesl_encrypt(
+		uint8_t crypto_method,
+		const char *src,
+		char *dst,
+		uint8_t len,
+		uint8_t maxlen
+		)
+{
+	if (crypto_method != MESL_CRYPTO_METHOD_USER7) {
+		// qCWarning(QGCMAVLinkLog) <<
+		//   "MESL_CRYPTO: invalid crypto method requested";
+		return (int32_t)-1;
+	}
+
+	// User defined encryption:
+	//   XOR with key '0xab', for test.
+	memcpy(
+			dst,
+			src,
+			len);
+	for (uint8_t i = (uint8_t)0; i < len; i++) {
+		dst[i] ^= 0xab;
+	}
+	return (int32_t)len;
+}
+
+// @brief  Function to decrypt MAVLink payload.
+//         Program that use MAVLink should implement this function.
+// @param  'crypto_method': method for decryption,
+//           value can be 'MESL_CRYPTO_METHOD_XXX'.
+//         For invalid 'crypto_method',
+//           this function should return '-1'.
+// @param  'len': payload length.
+//         If 'len' is zero,
+//           this function should return '-1'.
+// @return Payload length after decryption (zero is valid result).
+//         If "output_len < 0 || output_len > maxlen",
+//           will be considered as error,
+//           one example is for invalue 'crypto_method'.
+MAVLINK_HELPER int32_t mavlink_mesl_decrypt(
+		uint8_t crypto_method,
+		const char *src,
+		char *dst,
+		uint8_t len,
+		uint8_t maxlen
+		)
+{
+	if (crypto_method != MESL_CRYPTO_METHOD_USER7) {
+		return (int32_t)-1;
+	}
+
+	// User defined encryption:
+	//   XOR with key '0xab', for test.
+	memcpy(
+			dst,
+			src,
+			len);
+	for (uint8_t i = (uint8_t)0; i < len; i++) {
+		dst[i] ^= 0xab;
+	}
+	return (int32_t)len;
+}
+
+#endif // #ifdef MESL_CRYPTO
+
+#ifdef MAVLINK_USE_CXX_NAMESPACE
+} // namespace mavlink
+#endif
+
+
 
 #ifdef __GNUC__
 #	pragma GCC diagnostic pop
